5dad8d3a8818ca1638462b9813d1d3975c40f0e1
prajil <prajil@qburst.com>
2017-09-27 20:09:40 +0530

    User registration signup signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, format: { with: VALID_EMAIL_REGEX }
 end
